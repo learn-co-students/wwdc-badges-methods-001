9275682fea2860413e1210115515aee0df94a5e7
pri8771 <pchordia@andrew.cmu.edu>
2015-07-30 13:59:42 +0200

    WWDCB

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -19,21 +19,30 @@
 
 - (NSString *) makeBadgeForSpeaker: (NSString *)speaker {
     for (NSUInteger i = 0; i < 8; i++) {
-        NSString *names = [NSString stringWithFormat:@"Hello, my name is, %@", speaker];
+        NSString *names = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
         return names;
     }
     return 0;
 }
 
 - (NSMutableArray *) makeAllBadgesForSpeakers: (NSArray *)speakers {
-    for (NSUInteger i = 0; i < 8; i++) {
-        NSMutableArray *name = [NSMutableArray arrayWithObjects: @"Hello, my name is, %@", speakers, nil];
-        return name;
+    NSMutableArray *intro = [[NSMutableArray alloc] init];
+    for(NSUInteger i=0;i<8;i++) {
+        [intro addObject:[NSString stringWithFormat:@"Hello, my name is %@., ",speakers[i]]];
+    return intro;
     }
     return 0;
 }
 - (NSMutableArray *) greetAndAssignRoomsToSpeakers: (NSMutableArray *)speakers {
-    return nil;
+    NSMutableArray *fullIntro = [[NSMutableArray alloc] init];
+    for(NSUInteger i=0;i<8;i++) {
+        NSString *welcome = [NSString stringWithFormat:@"Welcome, %@!", speakers];
+        NSString *dressingRoom = [NSString stringWithFormat:@"You'll be in dressing room number %li.", i];
+        NSString *fullWelcome = [welcome stringByAppendingString:dressingRoom];
+        [fullIntro addObject:fullWelcome];
+        return fullIntro;
+            }
+    return 0;
 }
 
 @end
