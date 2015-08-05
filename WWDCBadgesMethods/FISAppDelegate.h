aeb7e6f8fd63005ba42a2a0f824ee148d256dab2
vicfriedman <victoria.m.friedman@gmail.com>
2015-08-05 10:52:50 -0400

    passed tests

--- WWDCBadgesMethods/FISAppDelegate.h
+++ WWDCBadgesMethods/FISAppDelegate.h
@@ -18,4 +18,18 @@
  
  */
 
+
+- (NSString *)makeBadgeForSpeaker:(NSString *)speaker;
+
+- (NSMutableArray *)makeAllBadgesForSpeakers:(NSArray *)speakers;
+
+- (NSMutableArray *)greetAndAssignRoomsToSpeakers:(NSArray *)speakers;
+
+
+
+
+
+
+
+
 @end
