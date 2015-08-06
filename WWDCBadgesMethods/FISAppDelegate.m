a66aaf418186bd1dbbea3d51af3f7524829a837e
laurrenreed <lauren.reed@me.com>
2015-08-06 00:03:30 -0700

    take 2

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -35,49 +35,49 @@ NSString *speaker = @"Inigo Montoya.";
     return badge; //return the badge
 }
 //speakers array
-//getting error "initilizer element is not a compile-time constant
-NSArray *speakers = @[@"Adele Goldberg.",
-                      @"Edsgar Dijkstra.",
-                      @"Joan Clarke.",
-                      @"Clarence Ellis.",
-                      @"Margaret Hamiliton.",
-                      @"George Boole.",
-                      @"Tim Bernes-Lee.",
-                      @"Jean Bartik."];
+//getting error "initilizer element is not a compile-time constant"
+//gave up and moved it inside of the individual methods
+//in googling it I read I apparently need to move the array into an initilizer .. not sure how to do that..?
+
 
 //makeBadgesForSpeakers method
 -(NSMutableArray *)makeAllBadgesForSpeakers:(NSArray *)speakers{ //return type mutable array, accepts an array called speakers
-    
-    NSMutableArray *badges[8]; //define a mutable array to store the badges
-    
-    for(NSUInteger i = 0; i < 8; i++){ //for loop to run through the speakers array
+    speakers = @[@"Adele Goldberg",
+                 @"Edsger Dijkstra",
+                 @"Joan Clarke",
+                 @"Clarence Ellis",
+                 @"Margaret Hamilton",
+                 @"George Boole",
+                 @"Tim Bernes-Lee",
+                 @"Jean Bartik"];
+    NSMutableArray *badges = [[NSMutableArray alloc] initWithCapacity:8];//define a mutable array to store the badges
+   
+        for(NSUInteger i = 0; i < 8; i++){ //for loop to run through the speakers array
         speaker = speakers[i]; //set speaker to speakers[i] so the makeBadgeForSpeaker method can accept each string of the array
-        [self makeBadgeForSpeaker:speaker]; //called the makeBadgeForSpeaker method internally to make a badge
-        [badges[i] addObject:speaker]; //add each return to the badges arrary
+        [badges addObject:[self makeBadgeForSpeaker:speaker]]; //add object to array from make badges array in one line
         NSLog(@"%@", badges[i]); //print out each line of the badges array
     
     }
     
-    return badges[7]; //return the mutable array called badges
+    return badges; //return the mutable array called badges
 }
 
 //greetingsAndRoomAssignments method
 -(NSMutableArray *)greetAndAssignRoomsToSpeakers:(NSArray *)speakers{ //return type mutable array, accepts an array called speakers
-    
+    speakers = @[@"Adele Goldberg",
+                 @"Edsger Dijkstra",
+                 @"Joan Clarke",
+                 @"Clarence Ellis",
+                 @"Margaret Hamilton",
+                 @"George Boole",
+                 @"Tim Bernes-Lee",
+                 @"Jean Bartik"];
     NSMutableArray *roomAssingments[8]; //define a mutable array to store the finished room assignments
-    
+    NSArray *roomNum = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8"]; //to store the room numbers
     for(NSUInteger i = 0; i < 8; i++){ //for loop to run through the speakers array
-        speaker = speakers[i]; //set speaker to speakers[i] so that i can append each string in the array
-        NSString *welcome = @"Welcome, "; //to store the first string to be appended
-        NSString *yourRoom = @"You'll be in room "; //to store the second string to be appended
-        NSArray *roomNum = @[@"1.", @"2.", @"3.", @"4.", @"5.", @"6.", @"7.", @"8."]; //to store the room numbers
-        
-        welcome = [welcome stringByAppendingString:speaker]; //"Welcome ..."
-        welcome = [welcome stringByReplacingOccurrencesOfString:@"." withString:@"!"];//Welcome ...!"
-        welcome = [welcome stringByAppendingString:yourRoom];//"Welcome ...! You'll be in room "
-        welcome = [welcome stringByAppendingString:roomNum[i]];//"Welcome ...! You'll be in room .."
-        [roomAssingments[i] addObject: welcome]; //add the final welcome string to the mutable array
-        
+        speaker = speakers[i];
+        NSString *yourRoom = [NSString stringWithFormat: @"Welcome %@! You'll be in room %@.", roomNum[i], speaker]; //to store the second string to be appended
+        [roomAssingments[i] addObject: yourRoom]; //add the final welcome string to the mutable array
         NSLog(@"%@", roomAssingments[i]); //print out each line of the mutable array
         
     }
