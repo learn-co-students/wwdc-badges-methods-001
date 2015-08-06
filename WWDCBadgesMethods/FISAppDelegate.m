04f437a0fa32232f81d147a20f27052f59104b59
adamecholaunch <abuys@colgate.edu>
2015-08-05 17:52:13 -0700

    Done

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "FISAppDelegate.h"
 
+
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
@@ -20,5 +21,47 @@
  * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
  
  */
+
+- (NSString *) makeBadgeForSpeaker:(NSString *)speaker {
+    
+    NSString *temp = @"Hello, my name is ";
+    temp = [temp stringByAppendingString:speaker];
+    return [temp stringByAppendingString: @"."];
+    
+}
+
+- (NSMutableArray *) makeAllBadgesForSpeakers:(NSArray *)speakers {
+   
+    NSMutableArray *result = [[NSMutableArray init] alloc];
+    
+    for (NSString *speaker in speakers) {
+        
+        NSString *badge = [self makeBadgeForSpeaker: speaker];
+       [result addObject: badge];
+    }
+    
+    return result;
+    
+}
+
+- (NSMutableArray *) greetAndAssignRoomsToSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *result = [[NSMutableArray init] alloc];
+    int count = 0;
+    
+    for (NSString *speaker in speakers) {
+        
+        NSString *first = @"Welcome, ";
+        NSString *second = [first stringByAppendingString:speaker];
+        NSString *next = @" You'll be in dressing room ";
+        NSString *following = [second stringByAppendingString:next];
+        NSString *notfinal = [following stringByAppendingString: [NSString stringWithFormat: @"%d", count]];
+        
+        NSString *final = [notfinal stringByAppendingString:@"."];
+        [result addObject: final];
+    }
+    
+    return result;
+}
 
 @end
