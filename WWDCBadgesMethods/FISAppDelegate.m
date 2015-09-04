ca38d19e64d119034989715f92bbab1da37fdb32
linda9009 <linda.ng.9009@gmail.com>
2015-09-04 05:00:12 -0400

    Using correct arrays

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -22,36 +22,26 @@
  */
 
 - (NSString *)badgeForSpeaker:(NSString *)speaker {
-    NSString *inigo = @"Inigo Montoya";
-    NSString *avenge = [NSString stringWithFormat:@"Hello, my name is %@.", inigo];
+    NSString *avenge = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
     return avenge;
 }
 
 - (NSArray *)badgesForSpeakers:(NSArray *)speakers {
-    NSArray *names = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
     
     NSMutableArray *badges = [[NSMutableArray alloc]init];
     
-    for (NSUInteger i=0; i < [names count]; i++) {
-        NSString *message = [NSString stringWithFormat:@"Hello, my name is %@.", names[i]];
+    for (NSUInteger i=0; i < [speakers count]; i++) {
+        NSString *message = [NSString stringWithFormat:@"Hello, my name is %@.", speakers[i]];
         [badges addObject:message];
     }
     return badges;
 }
 
-/*
- describe(@"greetAndAssignRoomsToSpeakers", ^{
- it(@"should return a list of welcome messages and room assignments",^{
- expect([appDelegate greetingsAndRoomAssignmentsForSpeakers:speakers]).to.equal(roomAssignments);
- 
- @"Welcome, Adele Goldberg! You'll be in dressing room 1."
- */
-
 - (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
-    NSArray *names = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    
     NSMutableArray *roomAssignments = [[NSMutableArray alloc] init];
-    for (NSUInteger i=0; i < [names count]; i++) {
-        NSString *message = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", names[i], i+1];
+    for (NSUInteger i=0; i < [speakers count]; i++) {
+        NSString *message = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speakers[i], i+1];
         [roomAssignments addObject:message];
     }
     return roomAssignments;
