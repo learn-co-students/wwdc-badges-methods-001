14fe58b522e98f4c0a2b09426be17144567ba560
linda9009 <linda.ng.9009@gmail.com>
2015-09-04 04:26:31 -0400

    Passed tests

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,40 @@
  
  */
 
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    NSString *inigo = @"Inigo Montoya";
+    NSString *avenge = [NSString stringWithFormat:@"Hello, my name is %@.", inigo];
+    return avenge;
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSArray *names = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    
+    NSMutableArray *badges = [[NSMutableArray alloc]init];
+    
+    for (NSUInteger i=0; i < [names count]; i++) {
+        NSString *message = [NSString stringWithFormat:@"Hello, my name is %@.", names[i]];
+        [badges addObject:message];
+    }
+    return badges;
+}
+
+/*
+ describe(@"greetAndAssignRoomsToSpeakers", ^{
+ it(@"should return a list of welcome messages and room assignments",^{
+ expect([appDelegate greetingsAndRoomAssignmentsForSpeakers:speakers]).to.equal(roomAssignments);
+ 
+ @"Welcome, Adele Goldberg! You'll be in dressing room 1."
+ */
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSArray *names = @[@"Adele Goldberg", @"Edsger Dijkstra", @"Joan Clarke", @"Clarence Ellis", @"Margaret Hamilton", @"George Boole", @"Tim Berners-Lee", @"Jean Bartik"];
+    NSMutableArray *roomAssignments = [[NSMutableArray alloc] init];
+    for (NSUInteger i=0; i < [names count]; i++) {
+        NSString *message = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", names[i], i+1];
+        [roomAssignments addObject:message];
+    }
+    return roomAssignments;
+}
+
 @end
