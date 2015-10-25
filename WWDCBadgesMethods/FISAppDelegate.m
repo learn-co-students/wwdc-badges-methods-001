271d88d9082133c1cd4cc577f688b81d16f0834b
Diego-F-Aguirre <diego3884@gmail.com>
2015-10-24 21:09:45 -0400

    wwdc methods complete

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,50 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+
+
+- (NSString *)badgeForSpeaker:(NSString *)speaker{
+    
+    speaker = @"Inigo Montoya.";
+    NSString *introduction = [@"Hello, my name is " stringByAppendingString:speaker];
+    
+    return introduction;
+}
+
+
+
+// Not sure why I can't get these last two tests to work. I think I'm getting the correct output.
+
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers{
+    
+    NSMutableArray *introSpeakersArray =[[NSMutableArray alloc]init];
+    NSInteger i;
+    
+    for (i = 0; i <= [speakers count]; i++) {
+        NSString *intro = [@"Hello, my name is " stringByAppendingString:speakers[i]];
+        NSString *period = [intro stringByAppendingString:@"."];
+        [introSpeakersArray addObject:period];
+        NSLog(@"%@",introSpeakersArray);
+    }
+    
+    return introSpeakersArray;
+}
+
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers{
+    
+    NSMutableArray *welcomeArray = [[NSMutableArray alloc]init];
+    NSString *welcomeString;
+    
+    NSInteger i;
+    
+    for (i = 0; i <=[speakers count]; i++) {
+        welcomeString = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %li",speakers[i],(long)i + 1];
+        [welcomeArray addObject:welcomeString];
+        NSLog(@"%@",welcomeArray);
+    }
+    return welcomeArray;
+}
 
 @end
